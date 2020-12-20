fbc6752e6eb24208c4281b8617dffed7ac7d1424
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-12-19 21:26:52 -0700

    added fetch event listener

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,7 +1,11 @@
 //Week 18, activity 12
 
 const FILES_TO_CACHE = [
-
+    "/",
+    "/index.js",
+    "/style.css",
+    "/index.html",
+    "/manifest.webmanifest"
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -34,3 +38,31 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
+//fetch
+self.addEventListener("fetch", function (evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
+    //week 18, activity 13
+    evt.respondWith(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
+        })
+    );
+});
